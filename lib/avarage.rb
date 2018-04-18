641da7b7af241e9b38ff3cb5f2c48a2bd4578325
itggot-isac-borghed <35598522+itggot-isac-borghed@users.noreply.github.com>
2018-04-18 14:43:46 +0200

    genomsnitt och fixat power.

---
+++ lib/avarage.rb
@@ -0,0 +1,4 @@
+require_relative 'sum'
+def avarage(array)
+    output sum(array)/array.length
+end
--- lib/power.rb
+++ lib/power.rb
@@ -1,4 +1,5 @@
 def power(base, exponent)
+    return 1 if exponent == 0
     output = base
     i = 1
     while i < exponent
